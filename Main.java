a41ace0acae34d71bf2dc0de54b74d563b4a9999
Rogach <platon7pronko@gmail.com>
2015-05-28 19:44:07 +0300

    move back to Swing-based gui

--- Main.java
+++ Main.java
@@ -1,96 +1,12 @@
-import javafx.application.Application;
-import javafx.event.*;
-import javafx.scene.*;
-import javafx.scene.canvas.*;
-import javafx.scene.image.*;
-import javafx.scene.control.*;
-import javafx.scene.layout.*;
-import javafx.scene.paint.*;
-import javafx.stage.Stage;
-import javafx.geometry.*;
+import javax.swing.*;
 
-public class Main extends Application {
-
-    public static int MAX_IMAGE_SIZE = 500;
+public class Main {
 
     public static void main(String[] args) throws Exception {
-        launch(new String[] {});
-    }
+        UIManager.setLookAndFeel("javax.swing.plaf.nimbus.NimbusLookAndFeel");
 
-    RenderCanvas renderCanvas;
-    Slider headingSlider;
-    Slider pitchSlider;
-
-    @Override
-    public void start(Stage stage) {
-        stage.setTitle("stc viewer");
-
-        GroupLayoutPane root = new GroupLayoutPane();
-        root.setStyle("-fx-background-color: #aaa");
-        root.setPadding(new Insets(5));
-
-        Button saveButton = new Button("Save image");
-        saveButton.setOnAction(event -> {
-                throw new UnsupportedOperationException("unimpl");
-            });
-
-        renderCanvas = new RenderCanvas();
-
-        headingSlider = new Slider(0, 360, 180);
-        headingSlider.setOrientation(Orientation.HORIZONTAL);
-        headingSlider.valueChangingProperty().addListener((e, wasChanging, isChanging) -> {
-                if (!isChanging) {
-                    updateRender();
-                }
-            });
-
-        pitchSlider = new Slider(-90, 90, 0);
-        pitchSlider.setOrientation(Orientation.VERTICAL);
-        pitchSlider.valueChangingProperty().addListener((e, wasChanging, isChanging) -> {
-                if (!isChanging) {
-                    updateRender();
-                }
-            });
-
-        root.getChildren().add(saveButton);
-        root.getChildren().add(renderCanvas);
-        root.getChildren().add(pitchSlider);
-        root.getChildren().add(headingSlider);
-
-        root.setHorizontalGroup(root.createParallelGroup()
-                                .addGroup(root.createSequentialGroup()
-                                          .addNode(saveButton, GroupLayoutPane.PREFERRED_SIZE)
-                                          .addGlue())
-                                .addGroup(root.createSequentialGroup()
-                                          .addNode(renderCanvas, 100, MAX_IMAGE_SIZE, MAX_IMAGE_SIZE)
-                                          .addGap(4)
-                                          .addNode(pitchSlider, 16))
-                                .addGroup(root.createSequentialGroup()
-                                          .addNode(headingSlider, 0, MAX_IMAGE_SIZE, MAX_IMAGE_SIZE)
-                                          .addGap(20)));
-
-        root.setVerticalGroup(root.createSequentialGroup()
-                              .addNode(saveButton)
-                              .addGap(5)
-                              .addGroup(root.createParallelGroup()
-                                        .addNode(renderCanvas, 100, MAX_IMAGE_SIZE, MAX_IMAGE_SIZE)
-                                        .addNode(pitchSlider, 0, MAX_IMAGE_SIZE, MAX_IMAGE_SIZE))
-                              .addGap(4)
-                              .addNode(headingSlider, 16));
-
-        Scene scene = new Scene(root);
-        stage.setScene(scene);
-        stage.show();
-        updateRender();
-    }
-
-    public void updateRender() {
-        RenderParams params = new RenderParams();
-        params.heading = Math.toRadians(headingSlider.getValue());
-        params.pitch = Math.toRadians(pitchSlider.getValue());
-        params.surfaceFile = "data/lh.inflated";
-        params.stcFile = "data/pas_45_kanizsa-lh.stc";
-        renderCanvas.updateRender(params);
+        MainFrame frame = new MainFrame();
+        frame.setVisible(true);
     }
 
 }
--- RenderPanel.java
+++ RenderPanel.java
@@ -1,23 +1,21 @@
-import javafx.scene.canvas.*;
-import javafx.embed.swing.SwingFXUtils;
-import java.awt.Graphics2D;
-import java.awt.Color;
-import java.awt.image.BufferedImage;
+import javax.swing.*;
+import java.awt.Graphics;
+import java.awt.image.*;
 
-public class RenderCanvas extends Canvas {
+public class RenderPanel extends JPanel {
 
     private RenderParams params;
 
     public void updateRender(RenderParams params) {
         this.params = params;
-        updateRender();
+        repaint();
     }
 
     private Renderer oldRenderer = null;
 
-    public void updateRender() {
+    @Override
+    public void paintComponent(Graphics g) {
         if (params != null) {
-            GraphicsContext g = this.getGraphicsContext2D();
             params.width = (int) this.getWidth();
             params.height = (int) this.getHeight();
             Renderer r = new Renderer(params, oldRenderer);
@@ -27,21 +25,11 @@ public class RenderCanvas extends Canvas {
                 long end = System.currentTimeMillis();
                 System.out.printf("rendering took %d ms\n", end - stt);
 
-                g.drawImage(SwingFXUtils.toFXImage(render, null), 0, 0);
+                g.drawImage(render, 0, 0, null);
                 oldRenderer = r;
             } catch (Exception e) {
                 e.printStackTrace();
             }
         }
     }
-
-    @Override
-    public void resize(double width, double height) {
-        if (this.widthProperty().get() != width || this.heightProperty().get() != height) {
-            this.widthProperty().set(width);
-            this.heightProperty().set(height);
-            updateRender();
-        }
-    }
-
 }
